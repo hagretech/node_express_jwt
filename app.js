66477fe08b6d0808a6603f57e5c96f322093437e
kirubel demelah <kiztopia.2020@gmail.com>
2021-01-11 19:26:15 -0500

    sign in view with full validator added

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const authRoutes = require('./routes/authRoutes');
-
+const cookieParser = require('cookie-parser');
 const app = express();
 
 // middleware
 app.use(express.static('public'));
 app.use(express.json());
+app.use(cookieParser());
 
 // view engine
 app.set('view engine', 'ejs');
@@ -21,5 +22,6 @@ mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCr
 app.get('/', (req, res) => res.render('home'));
 app.get('/smoothies', (req, res) => res.render('smoothies'));
 app.use(authRoutes);
+
 
 app.listen(3000, () => console.log('server is up'))
--- controllers/authController.js
+++ controllers/authController.js
@@ -1,55 +1,64 @@
 const User = require("../models/User");
+const jwt = require('jsonwebtoken')
+const maxAge = 3 * 24 * 60 * 60
+    // jwt creater
+const createToken = (id) => {
+    return jwt.sign({ id }, "kiztopia", {
+        expiresIn: maxAge
+    })
+}
 
 // handle errors
 const handleErrors = (err) => {
-  console.log(err.message, err.code);
-  let errors = { email: '', password: '' };
+    console.log(err.message, err.code);
+    let errors = { email: '', password: '' };
 
-  // duplicate email error
-  if (err.code === 11000) {
-    errors.email = 'that email is already registered';
+    // duplicate email error
+    if (err.code === 11000) {
+        errors.email = 'that email is already registered';
+        return errors;
+    }
+
+    // validation errors
+    if (err.message.includes('user validation failed')) {
+        // console.log(err);
+        Object.values(err.errors).forEach(({ properties }) => {
+            // console.log(val);
+            // console.log(properties);
+            errors[properties.path] = properties.message;
+        });
+    }
+
     return errors;
-  }
-
-  // validation errors
-  if (err.message.includes('user validation failed')) {
-    // console.log(err);
-    Object.values(err.errors).forEach(({ properties }) => {
-      // console.log(val);
-      // console.log(properties);
-      errors[properties.path] = properties.message;
-    });
-  }
-
-  return errors;
 }
 
 // controller actions
 module.exports.signup_get = (req, res) => {
-  res.render('signup');
+    res.render('signup');
 }
 
 module.exports.login_get = (req, res) => {
-  res.render('login');
+    res.render('login');
 }
 
-module.exports.signup_post = async (req, res) => {
-  const { email, password } = req.body;
+module.exports.signup_post = async(req, res) => {
+    const { email, password } = req.body;
 
-  try {
-    const user = await User.create({ email, password });
-    res.status(201).json(user);
-  }
-  catch(err) {
-    const errors = handleErrors(err);
-    res.status(400).json({ errors });
-  }
- 
+    try {
+        const user = await User.create({ email, password });
+        const token = createToken(user._id);
+        res.cookie('jwt', token, { httpOnly: true, maxAge: maxAge * 1000, });
+        res.status(201).json(user);
+    } catch (err) {
+        const errors = handleErrors(err);
+        res.status(400).json({ errors });
+    }
+
 }
 
-module.exports.login_post = async (req, res) => {
-  const { email, password } = req.body;
+module.exports.login_post = async(req, res) => {
+    const { email, password } = req.body;
 
-  console.log(email, password);
-  res.send('user login');
+    console.log(email, password);
+    res.send('user login');
 }
